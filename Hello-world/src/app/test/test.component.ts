3c54b3648f68cb52241837297338999db72cbd41
chandra-98a <chandrakalaa2022@gmail.com>
2023-10-03 12:02:48 +0530

    "Components in anular basics"

---
+++ Hello-world/src/app/test/test.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[app-test]',
+  template: '<div>Inline Template</div>',
+  styles: [`
+  div{
+    color:red;
+    }
+    `
+]
+})
+export class TestComponent {
+
+}

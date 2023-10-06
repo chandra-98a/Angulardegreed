08590fdf86d27de9a027ad69e7377ed40b09b946
chandra-98a <chandrakalaa2022@gmail.com>
2023-10-06 14:40:55 +0530

    "Template reference variable"

--- Binding/src/app/test/test.component.ts
+++ Binding/src/app/test/test.component.ts
@@ -3,25 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `<div> welcome {{name}}</div>
-<button (click)="onClick($event)">greet</button>
-<button (click)="greeting='welcome vishwas'">Here</button>
-{{greeting}}
+<input #myInput type="text"/>
+<button (click)="logMessage(myInput)">log</button>
   `,
   styles: []
 })
 export class TestComponent implements OnInit{
   public name="vishwas";
-public greeting="";
- /*  onClick(){
-console.log(`welcome to event binding`)
-this.greeting='welcome here';
-  } */
-onClick(event:any){
-console.log(event)
-this.greeting=event.type;
-}
   constructor(){}
-
+logMessage(value: any){
+console.log(value)
+}
   ngOnInit(){}
 
 }

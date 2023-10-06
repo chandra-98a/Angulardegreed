5dceb0bc62f29930d7be4096f228a40252287d3c
chandra-98a <chandrakalaa2022@gmail.com>
2023-10-06 12:53:29 +0530

    Style-binding

--- Binding/src/app/test/test.component.ts
+++ Binding/src/app/test/test.component.ts
@@ -3,51 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `<div> welcome {{name}}</div>
-  <h2 class="text-success">Namaste</h2>
- <h2 [class]="successClass">Namaste</h2>
-
-<h2 [class.text-danger]="hasError">Namaste</h2>
-<h2 [ngClass]="messageClass">Namaste</h2>
-
-  <!-- interpolation
-     <h2>{{2+2}}</h2>
-  <h2>{{"goodbye"+name}}</h2>
-  <h2>{{name.length}}</h2>
-  <h2>{{name.toUpperCase()}}</h2>
-  <h2>{{greetMessage()}} -->
-<!-- property binding
-<input [id]="myId" type="text" value="vishwas"/>
-<input bind-disabled="isDisabled" id="{{myId}}" type="text" value="vishwas"/> -->
+<h2 [style.color]="hasError ? 'red' : 'green' " >Style Binding</h2>
+<h2 [style.color]="highlightColor">Style property2</h2>
+<h2 [ngStyle]="titleStyles">Style Property3</h2>
   `,
-  styles: [`
-  .text-success{
-      color: green;
-    },
-    .text-danger{
-      color: red;
-    },
-    .text-special{
-      font-style: italic;
-    }`
-  ]
+  styles: []
 })
 export class TestComponent implements OnInit{
   public name="vishwas";
-  public successClass="text-success";
   public hasError=true;
-  public isSpecial=true;
-  public messageClass={
-    "text-success":!this.hasError,
-    "text-special":this.isSpecial,
-    "text-danger":this.hasError
+  public highlightColor="Orange";
+  public titleStyles={
+    color: "blue",
+    fontstyle: "italic"
   }
 
- /*  public myId="testId";
-  public isDisabled=true; */
   constructor(){}
 
   ngOnInit(){}
-/* greetMessage(){
-return "Hello"+this.name;
-  } */
+
 }

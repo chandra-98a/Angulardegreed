462fc785167310673dce4f4510e71dbe8044341a
chandra-98a <chandrakalaa2022@gmail.com>
2023-10-06 12:32:56 +0530

    "class-binding"

--- Binding/src/app/test/test.component.ts
+++ Binding/src/app/test/test.component.ts
@@ -3,25 +3,51 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `<div> welcome {{name}}</div>
-  <!--  <h2>{{2+2}}</h2>
+  <h2 class="text-success">Namaste</h2>
+ <h2 [class]="successClass">Namaste</h2>
+
+<h2 [class.text-danger]="hasError">Namaste</h2>
+<h2 [ngClass]="messageClass">Namaste</h2>
+
+  <!-- interpolation
+     <h2>{{2+2}}</h2>
   <h2>{{"goodbye"+name}}</h2>
   <h2>{{name.length}}</h2>
   <h2>{{name.toUpperCase()}}</h2>
   <h2>{{greetMessage()}} -->
-
+<!-- property binding
 <input [id]="myId" type="text" value="vishwas"/>
-<input bind-disabled="isDisabled" id="{{myId}}" type="text" value="vishwas"/>
+<input bind-disabled="isDisabled" id="{{myId}}" type="text" value="vishwas"/> -->
   `,
-  styles: []
+  styles: [`
+  .text-success{
+      color: green;
+    },
+    .text-danger{
+      color: red;
+    },
+    .text-special{
+      font-style: italic;
+    }`
+  ]
 })
 export class TestComponent implements OnInit{
-  public name="vishwas"
-  public myId="testId";
-  public isDisabled=true;
-  constructor(){}
-  greetMessage(){
-return "Hello"+this.name;
+  public name="vishwas";
+  public successClass="text-success";
+  public hasError=true;
+  public isSpecial=true;
+  public messageClass={
+    "text-success":!this.hasError,
+    "text-special":this.isSpecial,
+    "text-danger":this.hasError
   }
-  ngOnInit(){}
 
+ /*  public myId="testId";
+  public isDisabled=true; */
+  constructor(){}
+
+  ngOnInit(){}
+/* greetMessage(){
+return "Hello"+this.name;
+  } */
 }

3d0ec0aa835d6704808972c344589890783c8efd
chandra-98a <chandrakalaa2022@gmail.com>
2023-10-06 11:34:28 +0530

    "property binding"

--- Binding/src/app/test/test.component.ts
+++ Binding/src/app/test/test.component.ts
@@ -3,15 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `<div> welcome {{name}}</div>
-   <h2>{{2+2}}</h2>
+  <!--  <h2>{{2+2}}</h2>
   <h2>{{"goodbye"+name}}</h2>
   <h2>{{name.length}}</h2>
   <h2>{{name.toUpperCase()}}</h2>
-  <h2>{{greetMessage()}}`,
+  <h2>{{greetMessage()}} -->
+
+<input [id]="myId" type="text" value="vishwas"/>
+<input bind-disabled="isDisabled" id="{{myId}}" type="text" value="vishwas"/>
+  `,
   styles: []
 })
 export class TestComponent implements OnInit{
   public name="vishwas"
+  public myId="testId";
+  public isDisabled=true;
   constructor(){}
   greetMessage(){
 return "Hello"+this.name;

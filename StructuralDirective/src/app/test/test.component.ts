53ac13389d6a6a43ec84183505409bc4a78a1940
chandra-98a <chandrakalaa2022@gmail.com>
2023-10-16 15:45:02 +0530

    "ngFor"

--- StructuralDirective/src/app/test/test.component.ts
+++ StructuralDirective/src/app/test/test.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `
+  <div *ngFor="let color of colors; index as i; first as f">
+    <h2>{{i}}{{f}}{{color}}</h2>
+</div>
+
+  `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
-
+public colors=["red","blue","green","voilet"]
 }

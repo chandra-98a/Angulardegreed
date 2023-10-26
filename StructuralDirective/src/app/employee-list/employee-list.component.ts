ac73f232d2e9af099dc7bc25f2e03aeb53f37035
chandra-98a <chandrakalaa2022@gmail.com>
2023-10-26 16:27:48 +0530

    "http error handling in angular"

--- StructuralDirective/src/app/employee-list/employee-list.component.ts
+++ StructuralDirective/src/app/employee-list/employee-list.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit} from '@angular/core';
-import { IEmployee } from '../employee';
+import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from './../employee.service';
 
 @Component({
-  selector: 'app-employee-list',
+  selector: 'employee-list',
   template: `
-  <h2>Employee list</h2>
-  <ul *ngFor="let employee of employees">
-      <li>{{employee.id}}. {{employee.name}} - {{employee.age}}</li>
+    <h2>Employee List</h2>
+    <h3>{{errorMsg}}</h3>
+    <ul *ngFor="let employee of employees">
+      <li>{{employee.name}}</li>
     </ul>
   `,
-  styleUrls: ['./employee-list.component.css']
+  styles: []
 })
 export class EmployeeListComponent implements OnInit {
-public employees =[];
-constructor(private _employeeService: EmployeeService){}
 
-  ngOnInit(){
-this. _employeeService.getEmployees()
-.subscribe(data => this.employees = data);
+  public employees = [];
+  public errorMsg: any;
+  constructor(private _employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this._employeeService.getEmployees()
+      .subscribe(data => this.employees = data,
+                error => this.errorMsg = error);
   }
+
+
 
 }

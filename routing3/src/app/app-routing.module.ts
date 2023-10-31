f31a79e1e66f12b635633378e6b529d9023f2977
chandra-98a <chandrakalaa2022@gmail.com>
2023-10-31 15:35:44 +0530

    wildcard routes and redirecting routes

---
+++ routing3/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path:'',redirectTo:'/department-list', pathMatch:'full'},
+  {path:'employee-list',component:EmployeeListComponent},
+  {path:'department-list',component:DepartmentListComponent},
+  {path:"**",component:PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents=[
+  EmployeeListComponent,DepartmentListComponent
+]
---
+++ routing3/src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  template:`
+  <h3>page not found</h3>
+  `
+  ,
+  styleUrls: ['./page-not-found.component.css']
+})
+export class PageNotFoundComponent {
+
+}
